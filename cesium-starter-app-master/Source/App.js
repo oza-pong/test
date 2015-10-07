c62b03f3cc3d685d6b57ea6a132fb1e495c4530a
oza-pong <risei.heian.kashiba@gmail.com>
2015-10-07 15:04:13 +0900

    h

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://oza-pong.github.io/test/cesium-starter-app-master/Source/dov.kmz'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://oza-pong.github.io/test/cesium-starter-app-master/Source/doc.kml'));
